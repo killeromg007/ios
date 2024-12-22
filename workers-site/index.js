de6a51f7845cd236dea07f6a6fdbb818fa1527ec
killeromg007 <991127smalljames@gmail.com>
2024-12-22 12:57:46 +0800

    Initial commit

---
+++ workers-site/index.js
@@ -0,0 +1,18 @@
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
+
+addEventListener('fetch', event => {
+  event.respondWith(handleEvent(event))
+})
+
+async function handleEvent(event) {
+  try {
+    let response = await getAssetFromKV(event)
+    return response
+  } catch (e) {
+    let pathname = new URL(event.request.url).pathname
+    return new Response(`"${pathname}" not found`, {
+      status: 404,
+      statusText: 'not found',
+    })
+  }
+}
