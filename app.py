dc87c197aca732bae271cec253903a8640f595fc
killeromg007 <991127smalljames@gmail.com>
2024-12-22 15:03:30 +0800

    a
    a

--- app.py
+++ app.py
@@ -198,3 +198,7 @@ def logout():
 # Create tables
 with app.app_context():
     db.create_all()
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
